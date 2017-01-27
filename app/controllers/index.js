73570ff7233593efea4592b2f6ac63141c285b72
Daniel Toro <d.toro@outlook.com>
2017-01-27 02:07:06 -0300

    merge

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,11 +3,12 @@ angular.module('lazer',[])
 
 .controller('main', function( $scope, $http ){
 
-      $http.get('http://localhost:3000/api')
-      .then(function(data){
+      $http.get('http://localhost:4000/api')
+      .then(function (data){
 
-          $scope.datos = data.data;
-      },function(error){
-          //si hay un error lo mostramos por consola
-          console.log(error)
+        $scope.datos = data.data;
+      }, function (error) {
+        // si hay un error lo mostramos por consola
+        console.log(error)
+      })
 })
