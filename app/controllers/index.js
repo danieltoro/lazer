993aa9416a1e783fdf1c7a338f91b42f2ee52e3f
Daniel Toro <d.toro@outlook.com>
2017-01-27 16:05:11 -0300

    Actualizacion del proyecto

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,5 @@
 angular.module('lazer',[])
 
-
 .config(function($routeProvider){
 	$routeProvider
 	.when('/', {
@@ -9,27 +8,17 @@ angular.module('lazer',[])
 	})
 	.otherwise({
             redirectTo: '/'
-  })
-
+	})
 })
 .controller('main', function( $scope, $http ){
 
-<<<<<<< HEAD
-      $http.get('http://localhost:4000/api')
+
+      $http.get('http://localhost:3000/api/commerce')
       .then(function (data){
 
         $scope.datos = data.data;
       }, function (error) {
         // si hay un error lo mostramos por consola
         console.log(error)
-=======
-      $http.get('http://localhost:3000/api')
-      .then(function(data){
-
-          $scope.datos = data.data;
-      },function(error){
-          //si hay un error lo mostramos por consola
-          console.log(error)
->>>>>>> master
       })
 })
